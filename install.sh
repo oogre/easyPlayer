08f4f46b650e6487978ab1a904843703cbf74a11
oogre <vincent@ogre.be>
2021-10-12 15:35:12 +0200

    update install

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # @Author: Evrard Vincent
 # @Date:   2021-10-12 15:09:56
 # @Last Modified by:   oogre
-# @Last Modified time: 2021-10-12 15:32:06
+# @Last Modified time: 2021-10-12 15:35:00
 
 
-ln -s bin/play /usr/local/bin/play
-ln -s bin/omxplayer-sync /usr/local/bin/omxplayer-sync
-ln -s bin/customount /usr/local/bin/customount
-ln -s bin/getFirstAvailbaleAudioDeviceId /usr/local/bin/getFirstAvailbaleAudioDeviceId
-ln -s bin/easyPlayer /usr/local/bin/easyPlayer
+ln -s ~/easyPlayer/bin/play /usr/local/bin/play
+ln -s ~/easyPlayer/bin/omxplayer-sync /usr/local/bin/omxplayer-sync
+ln -s ~/easyPlayer/bin/customount /usr/local/bin/customount
+ln -s ~/easyPlayer/bin/getFirstAvailbaleAudioDeviceId /usr/local/bin/getFirstAvailbaleAudioDeviceId
+ln -s ~/easyPlayer/bin/easyPlayer /usr/local/bin/easyPlayer
 
