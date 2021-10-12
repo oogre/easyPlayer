d19ba752746451fe60bea15bf614e2c4b004af11
oogre <vincent@ogre.be>
2021-10-12 15:38:08 +0200

    update install

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # @Author: Evrard Vincent
 # @Date:   2021-10-12 15:09:56
 # @Last Modified by:   oogre
-# @Last Modified time: 2021-10-12 15:36:39
+# @Last Modified time: 2021-10-12 15:37:59
 
 
-ln -s home/pi/easyPlayer/bin/play /usr/local/bin/play
-ln -s home/pi/easyPlayer/bin/omxplayer-sync /usr/local/bin/omxplayer-sync
-ln -s home/pi/easyPlayer/bin/customount /usr/local/bin/customount
-ln -s home/pi/easyPlayer/bin/getFirstAvailbaleAudioDeviceId /usr/local/bin/getFirstAvailbaleAudioDeviceId
-ln -s home/pi/easyPlayer/bin/easyPlayer /usr/local/bin/easyPlayer
+ln -s /home/pi/easyPlayer/bin/play /usr/local/bin/play
+ln -s /home/pi/easyPlayer/bin/omxplayer-sync /usr/local/bin/omxplayer-sync
+ln -s /home/pi/easyPlayer/bin/customount /usr/local/bin/customount
+ln -s /home/pi/easyPlayer/bin/getFirstAvailbaleAudioDeviceId /usr/local/bin/getFirstAvailbaleAudioDeviceId
+ln -s /home/pi/easyPlayer/bin/easyPlayer /usr/local/bin/easyPlayer
 
